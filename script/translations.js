52095b68cf0a7f7d5430aa8d9a3f92517d62a94b
oltoit <166733190+oltoit@users.noreply.github.com>
2026-03-15 16:57:49 +0100

    test, wie gut das auf dem handy funktioniert

--- script/translations.js
+++ script/translations.js
@@ -1,5 +1,5 @@
 // standard language
-const langs = ["ger", "eng"];
+const langs = ["DE", "EN"];
 let lang_index = 0;
 
 // init function gatting called once
@@ -8,7 +8,8 @@ init();
 
 function init() {
     const langButton = document.getElementById("lang-button");
-    langButton,addEventListener("click", () => onChangeLang());
+    langButton.addEventListener("click", () => onChangeLang());
+    langButton.innerHTML = langs[lang_index];
 
     loadLang();
 }
@@ -17,24 +18,19 @@ function onChangeLang() {
     lang_index++;
     if (lang_index >= langs.length) {lang_index = 0};
 
-    const body = document.getElementById("body");
-    const langText = document.getElementById("lang-text");
-    body.removeChild(langText);
-
     loadLang();
+
+    const langButton = document.getElementById("lang-button");
+    langButton.innerHTML = langs[lang_index];
 }
 
 function loadLang() {
     const file = "lang/" + langs[lang_index] + ".html";
 
-    const body = document.getElementById("body");
-    const text = document.createElement("div");
-    text.setAttribute("id", "lang-text");
+    const text = document.getElementById("lang-text");
 
     fetch(file)
         .then(response => response.text())
         .then(respoText => text.innerHTML = respoText)
         .catch(err => console.error("Error while loading language", err));
-
-    body.appendChild(text);
 }
